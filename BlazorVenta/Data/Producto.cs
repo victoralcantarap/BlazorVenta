513e947ebac2ddd6434cf173f92e5617c6510176
Alex <Alex@SAMSUNG>
2024-06-06 22:25:56 -0400

    Agregar archivos de proyecto.

---
+++ BlazorVenta/Data/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorVenta.Data
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public int? Cantidad { get; set; }
+        public decimal? Precio { get; set; }
+        public DateOnly? Fecha { get; set; }
+        public decimal? Itbis{ get; set; }
+        public decimal? Total { get; set; }
+    }
+}
---
+++ BlazorVenta/Data/Respuesta.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorVenta.Data
+{
+    public class Respuesta<T>
+    {
+        public T Data { get; set; }
+    }
+}
